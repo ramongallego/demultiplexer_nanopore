a41d030db925760eb1aabecec26c89e847c3c7d4
ramongallego <ramongallego@gmail.com>
2022-08-21 15:12:43 +0200

    messages

--- plots.for.testing.anchors.r
+++ plots.for.testing.anchors.r
@@ -2,33 +2,66 @@ library(tidyverse)
 
 
 
-read_csv("~/Documents/test_demult/ouput.csv") %>% 
+read_csv("~/Documents/test_demult/ouput.csv") %>% View()
   pivot_longer(-threshold, names_to = "Step", values_to = "nReads") %>% 
-  mutate(Step = fct_relevel(Step, "start", "right.length","direction", "anchored", "direct_anchor")) %>% 
+  mutate (Anchoring = case_when(Step %in% c("direction", "anchored", "deplated_direction", "demult_twostep") ~ "Two_step",
+                                Step %in% c("direct_anchor", "deplated_direct", "demult_onestep")            ~  "One_step"   ),
+          Step = case_when(Step %in% c("anchored", "direct_anchor") ~ "Anchoring",
+                           str_detect(Step, "deplated") ~             "Deplating",
+                           str_detect(Step, "demult")   ~             "Demultiplexing",
+                           TRUE                         ~              Step)) %>% 
+  mutate(Step = fct_relevel(Step, "start", "right.length","direction", "Anchoring", "Deplating")) ->temp
+  temp %>% add_row(temp %>% filter (is.na(Anchoring)) %>% mutate(Anchoring = "One_step")) %>% 
+    replace_na(list(Anchoring= "Two_step")) %>% 
+    filter (Step != "reversed") %>% 
   ggplot(aes(x = Step, y = nReads, group = threshold, color = as.character(threshold))) +
-  geom_line()
+  geom_line() +
+  facet_wrap(~Anchoring, nrow =1) +
+    scale_y_continuous(limits = c(0,NA))
 
 
+### Each Step success rate
+  
+  temp %>% 
+    filter(!str_detect(Step ,"start|reversed")) -> for.success
 
-list.files("~/Projects/demultiplexer_nanopore/", pattern = "seq_") %>% map(~read_table(.x, col_names = c("length")) %>% 
-                                                                             mutate(file = .x)) %>% bind_rows() %>% 
+for.success %>% 
+  add_row(for.success %>% filter (is.na(Anchoring)) %>% replace_na(list(Anchoring = "One_step"))) %>% 
+  replace_na(list(Anchoring = "Two_step")) %>% 
+  rename(Number= Anchoring ) %>% 
+  pivot_wider(names_from = Step, values_from = nReads) %>% 
+  group_by(threshold, Number) %>% 
+summarise(Direction.finder = 100 * direction/right.length, 
+          Anchorage = 100 * Anchoring/right.length,
+          Deplating = 100 * Deplating/Anchoring,
+          Demultiplexing = 100 * Demultiplexing/Anchoring) %>% 
+  ggplot(aes())
+
+  
+  list.files(path = "~/Documents/test_demult/testing_anchors/", pattern = "seq_", full.names = T) %>% map(~read_table(.x, col_names = c("length")) %>% 
+                                                                             mutate(file = basename(.x))) %>% bind_rows() %>%
   mutate(Anchoring = case_when(str_detect(file, "directly") ~ "One-Step",
                                str_detect(file, "achored") ~ "Two-Step",
-                               str_detect(file, "direction") ~ "Two-Step"),
+                               str_detect(file, "one.step") ~ "One-Step",
+                               str_detect(file, "two.step") ~ "Two-Step"),
          Step = case_when(str_detect(file, "start") ~ "Start",
                           str_detect(file, "filter") ~ "Filtered",
                           str_detect(file, "deplated") ~ "First_demult",
                           str_detect(file, "direction") ~ "Direction",
-                          str_detect(file, "achored") ~ "Anchored"),
+                          str_detect(file, "achored") ~ "Anchored",
+                          str_detect(file, "demulted") ~ "Second_demult"),
          Step = fct_relevel(Step, "Start","Filtered", "Direction","Anchored"),
          threshold = str_extract(file, "[:digit:]..."),
          threshold = str_remove(threshold, "[:punct:]$")) -> df
-df %>% 
-  filter(Step == "Filtered")
-
 
 df %>% 
+  add_row(
+df %>% 
+  filter(str_detect(file,("_start|_filter"))) %>% 
+  mutate(Anchoring = "One-Step")) %>% 
+  replace_na(list(Anchoring = "Two-Step")) %>% 
   filter (!is.na(Anchoring)) %>% 
   ggplot(aes(x = length, after_stat(count))) +
   geom_density(aes(color = threshold), alpha = 0.5) +
-  facet_grid(Step~Anchoring, scales = "free_x")
+  facet_grid(Step~Anchoring, scales = "free_x") +
+  scale_x_continuous(limits = c(0,2000))
--- testing.anchor.sh
+++ testing.anchor.sh
@@ -1,10 +1,11 @@
 OUTPUT_SUMMARY=/home/mk1b/Documents/test_demult/ouput.csv
-printf "threshold,start,right.length,direction,reversed,anchored,
-direct_anchor,deplated_direction,deplated_direct, demult_twostep, demult_onestep\n" \
+printf "threshold,start,right.length,direction,reversed,anchored,direct_anchor,deplated_direction,deplated_direct, demult_twostep, demult_onestep, demult_rc\n" \
 > "${OUTPUT_SUMMARY}"
 READ1=/data/minknow/data/Run003_20220520_longAmplicons/no_sample/20220520_1400_MN40189_FAT20945_29cfc02a/fast5_pass/fastq_sup/all.passing.fastq
 DIRECTION=/home/mk1b/Documents/test_demult/demultiplexed_20220820_1059/
-OUTPUT_FOLDER=/home/mk1b/Documents/test_demult/testing_anchors
+OUTPUT_DIR=/home/mk1b/Documents/test_demult/testing_anchors
+START_TIME=$(date +%Y%m%d_%H%M)
+OUTPUT_FOLDER="${OUTPUT_DIR}"/demultiplexed_"${START_TIME}"
 mkdir "${OUTPUT_FOLDER}"
 cd "${OUTPUT_FOLDER}"
 cutadapt -m 1000 -M 2000 "${READ1}" -o "${READ1}".step1.fastq --quiet
@@ -28,7 +29,7 @@ echo
 awk 'NR % 4 == 2 {print length ($0)}' "${READ1}" > seq_lengths_start.txt
 awk 'NR % 4 == 2 {print length ($0)}' "${READ1}".step1.fastq > seq_lengths_filter.txt
 
-for threshold in 0.2 0.3 0.35; do
+for threshold in 0.2 0.3; do
   # Start by keeping only the reads of reasonable lengths
 
 
@@ -83,12 +84,15 @@ tot_demult_anchored=0
 tot_demult_direct=0
 tot_deplat_anchored=0
 tot_deplat_direct=0
+tot_demult_anchored_back=0
 
   for file in  two.step_"${threshold}"_*_round1.fastq; do
 
+    basename=$(echo "${file}" | sed 's/_round1.fastq//g'  )
+
      Seqs_deplat=$(awk 'NR %4 == 2' $file | wc -l)
      tot_deplat_anchored=$(expr "${tot_deplat_anchored}" + "${Seqs_deplat}")
-     awk 'NR % 4 == 2 {print length ($0)}' "${file}" > seq_lengths_deplated_anchored_direction_"${file}"_"${threshold}".txt
+     awk 'NR % 4 == 2 {print length ($0)}' "${file}" >> seq_lengths_deplated_anchored_direction_"${basename}".txt
 
      cutadapt -a ATCTCGTATGCCGTCTTCTGCTTG \
               -o  "${file}".short.fastq \
@@ -97,20 +101,22 @@ tot_deplat_direct=0
 
 
      cutadapt -a file:"${DIRECTION}"/noprimers/all.passing.fastq/A7/barcodes.p7.strict.fasta \
-      -o "${file}"_{name}_round2.fastq \
+      -o "${basename}"_{name}_round2.fastq \
       --quiet --untrimmed-output directly_nop7.fastq -e "${threshold}" "${file}".short.fastq
 
-      Seqs_demult=$(awk 'NR %4 == 2' directly_"${file}"_*_round2.fastq | wc -l)
+      Seqs_demult=$(awk 'NR %4 == 2' "${basename}"_*_round2.fastq | wc -l)
       tot_demult_anchored=$(expr "${tot_demult_anchored}" + "${Seqs_demult}")
-      awk 'NR % 4 == 2 {print length ($0)}' directly_"${file}"_*_round2.fastq > seq_lengths_demulted_anchored_two.step_"${file}".txt
+      awk 'NR % 4 == 2 {print length ($0)}' "${basename}"_*_round2.fastq >> seq_lengths_demulted_anchored_two.step_"${basename}".txt
 
    done
    #Now direct
    for file in  one.step_"${threshold}"_*_round1.fastq; do
 
+     basename=$(echo "${file}" | sed 's/_round1.fastq//g'  )
+
       Seqs_deplat=$(awk 'NR %4 == 2' $file | wc -l)
       tot_deplat_direct=$(expr "${tot_deplat_direct}" + "${Seqs_deplat}")
-      awk 'NR % 4 == 2 {print length ($0)}' "${file}" > seq_lengths_deplated_anchored_one.step_"${file}"_"${threshold}".txt
+      awk 'NR % 4 == 2 {print length ($0)}' "${file}" >> seq_lengths_deplated_anchored_one.step_"${basename}".txt
       cutadapt -a ATCTCGTATGCCGTCTTCTGCTTG \
                -o  "${file}".short.fastq \
         "${file}"  --discard-untrimmed --quiet -e "${threshold}"
@@ -118,20 +124,37 @@ tot_deplat_direct=0
 
 
       cutadapt -a file:"${DIRECTION}"/noprimers/all.passing.fastq/A7/barcodes.p7.strict.fasta \
-       -o "${file}"_{name}_round2.fastq \
+       -o "${basename}"_{name}_round2.fastq \
        --quiet --untrimmed-output directly_nop7.fastq -e "${threshold}" "${file}".short.fastq
 
-       Seqs_demult=$(awk 'NR %4 == 2' directly_"${file}"_*_round2.fastq | wc -l)
+       Seqs_demult=$(awk 'NR %4 == 2' "${basename}"_*_round2.fastq | wc -l)
        tot_demult_direct=$(expr "${tot_demult_direct}" + "${Seqs_demult}")
-       awk 'NR % 4 == 2 {print length ($0)}' "${file}"_*_round2.fastq > seq_lengths_demulted_anchored_one.step_"${file}".txt
+       awk 'NR % 4 == 2 {print length ($0)}' "${basename}"_*_round2.fastq >> seq_lengths_demulted_anchored_one.step_"${basename}".txt
+
+## Seems like the second demultiplexing step is the worst performer, with only 55-60% success. Lets do it with anchors as well
+
+seqkit seq -r -p -t DNA "${file}" -o "${basename}".rc.fastq
+
+
+  cutadapt -g CAAGCAGAAGACGGCATACGAGAT \
+         -o  "${basename}".anchored.rc.fastq \
+  "${basename}".rc.fastq  --discard-untrimmed --quiet -e "${threshold}"
+
+  cutadapt -g file:"${DIRECTION}"/noprimers/all.passing.fastq/A7/barcodes.p7.rc.anchored.fasta \
+  -o "${basename}"_{name}_round2_rc.fastq \
+  --quiet --untrimmed-output directly_nop7.fastq -e "${threshold}" "${file}".short.fastq
+
+  Seqs_demult=$(awk 'NR %4 == 2' "${basename}"_*_round2_rc.fastq | wc -l)
+  tot_demult_anchored_back=$(expr "${tot_demult_anchored_back}" + "${Seqs_demult}")
+  awk 'NR % 4 == 2 {print length ($0)}' "${basename}"_*_round2_rc.fastq >> seq_lengths_demulted_anchored_one.step_rc_"${basename}".txt
 
     done
 
-    printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" \
+    printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" \
     "${threshold}" "${Start}" "${Good_length}" "${Direction_check}"\
     "${Reversed}" "${Anchored}" "${Direct_Anchor}" \
     "${tot_deplat_anchored}" "${tot_deplat_direct}" \
-    "${tot_demult_anchored}" "${tot_demult_direct}">> "${OUTPUT_SUMMARY}"
+    "${tot_demult_anchored}" "${tot_demult_direct}" "${tot_demult_anchored_back}">> "${OUTPUT_SUMMARY}"
 
     # Length distributions
 
